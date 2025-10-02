97056b318c97d38ed4e9b2f066278afbfd8f114c
lovin <jean.l.ff@gmail.com>
2025-10-01 22:35:12 -0300

    contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,78 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Text('Lovin é o caralho!'),
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void decrement() {
+    print('decrement');
+  }
+
+  void increment() {
+    print('increment');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'cada click um !', 
+           style: TextStyle(
+            fontSize: 26, 
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+           ),
+          ),
+          const Text(
+            '0',
+            style: TextStyle(
+              fontSize: 100,
+              color: Colors.white,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: decrement, 
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                ),
+                child: Text('Saiu', style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 16),)
+              ),
+              TextButton(
+                onPressed: increment,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  fixedSize: const Size(100, 100),
+                ),
+                child: Text('Entrou', style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 16),)
+              ),
+            ],
+          ),
+        ]
         ),
-      ),
     );
   }
 }
